db1da8e0ed5aaca45cc46ab46b00b58861f2dd52
Sergei Shabanov <shabanov1989@gmail.com>
2022-07-20 16:24:13 +0300

    feat: добавил валидацию при регистрации

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -4,8 +4,15 @@ import { Link } from 'react-router-dom';
 const Register = (props) => {
   const [formParams, setFormParams] = useState({
     email: '',
-    password: ''
+    password: '',
+    confirmPassword: ''
   });
+
+  const [error, setError] = useState({
+    username: '',
+    password: '',
+    confirmPassword: ''
+  })
 
   const handleChange = (e) => {
     const {name, value} = e.target;
@@ -13,12 +20,58 @@ const Register = (props) => {
       ...prev,
       [name]: value
     }));
+    validateInput(e);
+  }
+
+  const validateInput = e => {
+    let { name, value } = e.target;
+    setError(prev => {
+      const stateObj = { ...prev, [name]: "" };
+
+      switch (name) {
+        case "email":
+          if (!value) {
+            stateObj[name] = "Введите email";
+          }
+          break;
+
+        case "password":
+          if (!value) {
+            stateObj[name] = "Введите пароль";
+          } else if (formParams.confirmPassword && value !== formParams.confirmPassword) {
+            stateObj["confirmPassword"] = "Пароли не совпадают";
+          } else {
+            stateObj["confirmPassword"] = formParams.confirmPassword ? "" : error.confirmPassword;
+          }
+          break;
+
+        case "confirmPassword":
+          if (!value) {
+            stateObj[name] = "Подтвердите пароль";
+          } else if (formParams.password && value !== formParams.password) {
+            stateObj[name] = "Пароли не совпадают";
+          }
+          break;
+
+        default:
+          break;
+      }
+
+      return stateObj;
+    });
   }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const { password, email } = formParams;
-    props.handleRegister({ password, email })
+    const { email, password, confirmPassword } = formParams;
+
+    if (password !== confirmPassword) {
+      return;
+    }
+    props.handleRegister({ email, password })
+      .catch(err => {
+        console.log(err.message)
+      })
   }
 
   return(
@@ -44,6 +97,20 @@ const Register = (props) => {
             placeholder="Пароль"
             onChange={handleChange}
           />
+          <div className="popup__text-container">
+            {error.password && <span className='popup__text-error'>{error.password}</span>}
+          </div>
+          {formParams.password && <input
+            className="register__text"
+            required
+            name="confirmPassword"
+            type="password"
+            placeholder="Подтвердите пароль"
+            onChange={handleChange}
+          />}
+          <div className="popup__text-container">
+            {formParams.password && error.confirmPassword && <span className='popup__text-error'>{error.confirmPassword}</span>}
+          </div>
           <div className="register__button-container">
             <button type="submit" className="register__link">Зарегистрироваться</button>
             <p className="register__signin-text">Уже зарегистрированы? <Link to="/signin" className="register__signin">Войти</Link></p>
