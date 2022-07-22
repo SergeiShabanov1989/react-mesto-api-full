7f76994f4776ffe06cce66bb48baf279292cb311
Sergei Shabanov <shabanov1989@gmail.com>
2022-07-22 13:44:47 +0300

    feat: добавил валидацию для данных о пользователя

--- frontend/src/components/EditProfilePopup.js
+++ frontend/src/components/EditProfilePopup.js
@@ -1,31 +1,86 @@
 import PopupWithForm from "./PopupWithForm";
 import React from "react";
 import {CurrentUserContext} from "../contexts/CurrentUserContext";
+import {useState} from "react";
 
 function EditProfilePopup(props) {
   const { currentUser } = React.useContext(CurrentUserContext)
-  const [name, setName] = React.useState('');
-  const [description, setDescription] = React.useState('');
+  // const [name, setName] = React.useState('');
+  // const [description, setDescription] = React.useState('');
+  const [dataUser, setDataUser] = useState({
+    name: '',
+    occupation: ''
+  });
+  const [error, setError] = useState({
+    name: '',
+    occupation: ''
+  })
 
   React.useEffect(() => {
-    setName(currentUser.name || '');
-    setDescription(currentUser.about || '');
+    setDataUser({
+      name: currentUser.name || '',
+      occupation: currentUser.about || ''
+    });
   }, [currentUser, props.onUpdateUser]);
 
-  function handleNameEdit(e) {
-    setName(e.target.value)
+  // function handleNameEdit(e) {
+  //   setName(e.target.value)
+  // }
+  //
+  // function handleDescriptionEdit(e) {
+  //   setDescription(e.target.value)
+  // }
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setDataUser((prev) => ({
+      ...prev,
+      [name]: value
+    }));
+    validateInput(e);
   }
 
-  function handleDescriptionEdit(e) {
-    setDescription(e.target.value)
+  const validateInput = e => {
+    let { name, value } = e.target;
+    setError(prev => {
+      const stateObj = { ...prev, [name]: "" };
+
+      switch (name) {
+        case "name":
+          if (!value) {
+            stateObj[name] = "Заполните имя";
+          } else if (dataUser.name.length < 1) {
+            stateObj["name"] = "Длина не может быт меньше двух символов";
+          } else {
+            stateObj["name"] = dataUser.name ? "" : error.name;
+          }
+          break;
+
+        case "occupation":
+          if (!value) {
+            stateObj[name] = "Заполните род деятельности";
+          } else if (dataUser.occupation.length < 1) {
+            stateObj["occupation"] = "Длина не может быт меньше двух символов";
+          } else {
+            stateObj["occupation"] = dataUser.occupation ? "" : error.occupation;
+          }
+          break;
+
+        default:
+          break;
+      }
+
+      return stateObj;
+    });
   }
 
   function handleSubmit(e) {
+    const { name, occupation } = dataUser;
     e.preventDefault();
 
     props.onUpdateUser({
       name: name,
-      about: description
+      about: occupation
     });
   }
 
@@ -44,13 +99,13 @@ function EditProfilePopup(props) {
           maxLength="40"
           required
           placeholder="Имя"
-          name="popup__name"
+          name="name"
           className="popup__text popup__text_type_name"
-          onChange={handleNameEdit}
-          value={name}
+          onChange={handleChange}
+          value={dataUser.name}
         />
         <div className="popup__text-container">
-          <span className="popup__text-error popup__name-error"></span>
+          {error.name && <span className='popup__text-error'>{error.name}</span>}
         </div>
       </div>
       <div className="popup__input-container">
@@ -60,13 +115,13 @@ function EditProfilePopup(props) {
           maxLength="200"
           required
           placeholder="Род деятельности"
-          name="popup__occupation"
+          name="occupation"
           className="popup__text popup__text_type_occupation"
-          onChange={handleDescriptionEdit}
-          value={description}
+          onChange={handleChange}
+          value={dataUser.occupation}
         />
         <div className="popup__text-container">
-          <span className="popup__text-error popup__occupation-error"></span>
+          {error.occupation && <span className='popup__text-error'>{error.occupation}</span>}
         </div>
       </div>
     </PopupWithForm>
